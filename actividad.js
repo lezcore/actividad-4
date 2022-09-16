deec03eb180e6765205884ae5004df33a427ba78
Sergio <69374674+lezcore@users.noreply.github.com>
2022-09-15 21:33:42 -0500

    Create actividad.js

---
+++ actividad.js
@@ -0,0 +1,15 @@
+// BIANOR SERGIO CHIPILE CARI
+
+// Numeración sucesiva
+for (i=0; i<=30; i++){
+  console.log('Numeracion sucesiva ' + i);
+}
+
+// Numero maximo
+function maxNumber(num1 , num2) {
+  if ( num1 > num2){
+    return num1;
+  }
+  return num2;
+}
+console.log('El numero máximo es ' + maxNumber(500,100));
